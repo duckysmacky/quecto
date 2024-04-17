ed430c4b269a17f3ad7181904d3374fd6fa28dbf
Ducky <kriperoka@gmail.com>
2024-04-17 22:43:28 +0300

    put all defs into seperate header

--- src/main.c
+++ src/main.c
@@ -5,52 +5,7 @@
 	(c) shroomwastaken 2024
 */
 
-#ifdef _WIN32
-#include <ncurses/curses.h>
-#include <stdio.h>
-#else
-#include <curses.h>
-#endif
-
-#include <assert.h>
-#include <string.h>
-#include <stdlib.h>
-
-// key defines
-#define ctrl(x) ((x) & 0x1f)
-
-#define ESCAPE 27
-#define ENTER 10
-#define TAB 9
-
-// ctrl + arrow has to be compared with the keyname() function
-#define CTRLUP "kUP5"
-#define CTRLDOWN "kDN5"
-#define CTRLLEFT "kLFT5"
-#define CTRLRIGHT "kRIT5"
-
-// limit defines
-#define MAX_ROWS 1024
-#define MAX_STRING_SIZE 1025
-
-#define ESCDELAY 10
-
-/* DATA */
-
-typedef struct ROW {
-	size_t index;
-	size_t size;
-	char* contents;
-} row_t;
-
-typedef struct BUFFER {
-	row_t rows[MAX_ROWS];
-	size_t row_index;
-	size_t cur_pos;
-	size_t row_s;
-	char* filename;
-} buffer_t;
-
+#include "include/main.h"
 
 struct STATE {
 	int y, x;
